bfd0f329407a9a2251d0c16a33479557b2520602
Laboratorios <Laboratorios@E02>
2018-11-17 10:35:14 -0600

    DataBase

---
+++ Progra4.AW.Products/Progra4.AW.Model/Mis Tablas/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra4.AW.Model
+{
+    public partial class Product
+    {
+        public string SizeInInches {
+            get {
+               string elResultado = Size;
+                if (SizeUnitMeasureCode!=null&& SizeUnitMeasureCode == "CM")
+                {
+                    float elTamaño = 0;
+                    if (float.TryParse(Size, out elTamaño))
+                    {
+                        elResultado = (elTamaño / (float)2.54).ToString();
+                    }
+                    
+                }
+                return elResultado;
+            }
+        }
+    }
+}
