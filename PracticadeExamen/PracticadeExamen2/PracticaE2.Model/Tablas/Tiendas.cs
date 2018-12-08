2d8c5e8d82dcbb92ec17f3c6735ed52311d78fc5
Daniela Alfaro <daniela_alfaro23@hotmail.com>
2018-12-08 10:41:51 -0600

    practicadeexamen
    practica

---
+++ PracticadeExamen/PracticadeExamen2/PracticaE2.Model/Tablas/Tiendas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaE2.Model
+{
+    public partial class Tiendas
+    {
+        //Se concatena la Provincia, el cantón y el distrito, separados por guiones
+        //y finalizado por un punto. Cuando el campo Distrito viene en nulo, no debe presentarse un
+        //guión al final.  
+    }
+}
---
+++ PracticadeExamen/PracticadeExamen2/PracticaE2.Model/Tablas/Zapatos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaE2.Model.Tablas
+{
+    class Class1
+    {
+    }
+}
